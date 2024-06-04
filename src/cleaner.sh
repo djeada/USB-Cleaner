0841e2be86cc4a23d8d18941d04488fd866cc9f7
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2024-06-04 23:38:39 +0200

    Update cleaner.sh

--- src/cleaner.sh
+++ src/cleaner.sh
@@ -1,97 +1,97 @@
 #!/usr/bin/env bash
 
 # Script Name: cleaner.sh
-# Description: A simple script to wipe USB drives. Caution: The user must have sudo privileges.
+# Description: A professional script to securely wipe USB drives and optionally create new partitions. 
+#              Caution: The user must have sudo privileges.
 # Usage: ./cleaner.sh
 
+# Function to display all USB drives found on the system and let the user choose one.
 get_usb_drive() {
-    # Display all USB drives found on the system and let the user choose one.
     local message="$1"
 
     echo "The following USB drives were found:"
-    
     ls -l /dev/disk/by-id/usb*
 
-    echo "$message You have to use the full path:"
-    usb_drive=""
-    read usb_drive
+    echo "$message Please provide the full path of the USB drive:"
+    read -r usb_drive
 
     echo "You have chosen $usb_drive. Type 'y' to confirm, or 'n' to cancel."
-    confirm=""
-    read confirm
-
+    read -r confirm
 }
 
+# Function to securely wipe the selected USB drive.
 wipe_disk() {
-    # Wipe a disk.
-
-    if [ $confirm == 'y' ]; then
+    if [ "$confirm" == 'y' ]; then
         echo "Cleaning $usb_drive"
-        sudo dd if=/dev/zero of=$usb_drive bs=1k count=2048
-        echo "Done!"
+        sudo dd if=/dev/zero of="$usb_drive" bs=1k count=2048 status=progress
+        echo "Wipe completed!"
+
         echo "Do you want to create a new partition on $usb_drive? Type 'y' to confirm, or 'n' to cancel."
-        read confirm
-        if [ $confirm == 'y' ]; then
+        read -r confirm
+
+        if [ "$confirm" == 'y' ]; then
             create_partition
         fi
     else
-        echo "Cancelled."
+        echo "Operation cancelled."
         return 1
     fi
 
     return 0
 }
 
+# Function to create a new partition on the wiped USB drive.
 create_partition() {
-    # Create a partition on a disk.
-
-      if [ $confirm == 'y' ]; then
+    if [ "$confirm" == 'y' ]; then
         local partition="${usb_drive}1"
-        sudo parted $usb_drive mklabel msdos
-        sudo parted -a none $usb_drive mkpart  primary fat32 0 2048
-        sudo mkfs.vfat -n "Disk" $partition
-        echo "Done!"
+        sudo parted "$usb_drive" mklabel msdos
+        sudo parted -a none "$usb_drive" mkpart primary fat32 0 2048
+        sudo mkfs.vfat -n "Disk" "$partition"
+        echo "Partition creation completed!"
     else
-        echo "Cancelled."
+        echo "Operation cancelled."
         return 1
     fi
 
     return 0
 }
 
+# Main function to drive the script.
 main() {
-
-    # check if sudo rights are available
+    # Check if the user has sudo privileges.
     sudo -v
-    if [[ $? -ne 0 ]] ; then
-        echo "You need to be in the sudoers group to run this script."
+    if [[ $? -ne 0 ]]; then
+        echo "You need to have sudo privileges to run this script."
         exit 1
     fi
 
-    # if the user provided a USB drive, use it.
+    # If a USB drive is provided as an argument, use it.
     if [ $# -eq 1 ]; then
         usb_drive=$1
         wipe_disk
         return
     elif [ $# -gt 1 ]; then
-        echo "Too many arguments."
+        echo "Too many arguments provided."
         exit 1
     fi
 
-    # display menu and get user input
-    echo "1. Wipe your disk clean."
-    echo "2. Create a new partition on your disk."
+    # Display menu options and get user input.
+    echo "Select an option:"
+    echo "1. Wipe your USB drive clean."
+    echo "2. Create a new partition on your USB drive."
     echo "3. Exit."
 
-    read choice
+    read -r choice
 
     case $choice in
-        1) get_usb_drive "Choose the drive to clean." && wipe_disk ;;
-        2) get_usb_drive "Choose the drive to create a partition on." && create_partition ;;
-        3) exit ;;
-        *) echo "Invalid choice. Please try again."
+        1) get_usb_drive "Choose the drive to clean:" && wipe_disk ;;
+        2) get_usb_drive "Choose the drive to create a partition on:" && create_partition ;;
+        3) exit 0 ;;
+        *) 
+            echo "Invalid choice. Please try again."
             main ;;
     esac
 }
 
+# Execute the main function with all script arguments.
 main "$@"
